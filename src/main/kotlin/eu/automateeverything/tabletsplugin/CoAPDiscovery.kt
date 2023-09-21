fccaa8c083f5e2d8fb353cb8870fe0607a9dd9f0
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2023-09-21 12:39:58 +0200

    CoAP discovery (CoRE)

--- src/main/kotlin/eu/automateeverything/tabletsplugin/CoAPDiscovery.kt
+++ src/main/kotlin/eu/automateeverything/tabletsplugin/CoAPDiscovery.kt
@@ -2,25 +2,31 @@ package eu.automateeverything.tabletsplugin
 
 import org.eclipse.californium.core.CoapClient
 import org.eclipse.californium.core.CoapResponse
+import org.eclipse.californium.core.coap.LinkFormat
 import java.net.InetAddress
 
 class CoAPDiscovery {
-    fun discover(address: InetAddress) {
-        val multicastPort = 5683
+    private val multicastPort = 5683
 
+    fun discover(address: InetAddress) {
         val client = CoapClient("coap:/${address}:$multicastPort/.well-known/core")
         client.timeout = 5000
 
-        val response: CoapResponse = client.get()
+        try {
+            val response: CoapResponse? = client.get()
 
-        if (response.isSuccess) {
-            // Print the response payload (CoRE Link Format)
-            println("CoAP Discovery Response:")
-            System.out.println(response.getResponseText())
-        } else {
-            System.err.println("Error: Unable to perform CoAP multicast request")
+            if (response != null && response?.isSuccess) {
+                println("CoAP Discovery Response:")
+                val links = LinkFormat.parse(response.responseText)
+                links.forEach {
+                    println(it.uri)
+                }
+            } else {
+                System.err.println("Error: Unable to perform CoAP multicast request")
+            }
+        } catch (ignored:Exception) {
+        } finally {
+            client.shutdown()
         }
-
-        client.shutdown()
     }
 }
